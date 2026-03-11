f16b3060019e8e029c114b06f8cfddb802f3f864
Miguel Londoño <magnolinneo1971@gmail.com>
2026-03-10 20:23:56 -0500

    Se crean los objetos para la clase padre Libro y las clases hijas

--- src/main/java/com/parcial/Main.java
+++ src/main/java/com/parcial/Main.java
@@ -6,16 +6,19 @@ public class Main {
         libro libro1 = new libro("El Quijote", "Miguel de Cervantes", 5, 0);
         libro1.prestamo();
         libro1.prestamo();
-        libro1.prestamo();
-        libro1.prestamo();
-        libro1.prestamo();
-        libro1.prestamo();
-
-        
+        libro1.devolucion();
         libro1.imprimir();
 
+        libro libro2 = new libro();
+        libro2.setTitulo("Mamba Mentality");
+        libro2.setAutor("Kobe Bryant");
+        libro2.setNumero_ejemplares(3);
+        libro2.setNumero_ejemplares_prestados(0);
 
+        libroTextoUniajc libroTextoUniajc1 = new libroTextoUniajc("Codigo Limpio", "Robert C. Martin", 2, 0, "Programacion", "Ingenieria");
+        libroTextoUniajc1.imprimir();
 
-        
+        novela novela1 = new novela("Cien años de soledad", "Gabriel García Márquez", 4, 0, "Realismo");
+        novela1.imprimir();
     }
 }
